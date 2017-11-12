a172c95daa121124ae914ad398a3cd65c6fec1df
Jean-Paul REMAN <jp.reman@obvy-app.com>
2017-11-12 13:10:13 +0100

    add GET / todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose'); 
+const { ObjectID } = require('mongodb');
 var {Todo} = require('./db/models/todo');
 var {User} = require('./db/models/user');
 
@@ -29,8 +30,23 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (err) => {
         res.status(400).send(err);
-    })
-})
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+        Todo.findById(id).then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo});
+        }, (err) => {
+            res.status(400).send(err);
+        });
+});
 
 app.listen(3000, () => {
     console.log('le serveur écoute le port 3000');
