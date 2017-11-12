631f9213d55f8a9c7926e4fcc78abd9581449522
Jean-Paul REMAN <jp.reman@obvy-app.com>
2017-11-12 17:32:40 +0100

    add properties to fix error on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
-    useMongoClient: true,
+    keepAlive: true,
+    reconnectTries: Number.MAX_VALUE,
+    useMongoClient: true
 });
 
 
