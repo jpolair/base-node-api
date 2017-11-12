65ea7b33f14953f0da9a66af152f8a75d1d24522
Jean-Paul REMAN <jp.reman@obvy-app.com>
2017-11-12 17:57:09 +0100

    update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,7 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{useMongoClient: true });
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
-    useMongoClient: true
-});
 
 
 module.exports = { mongoose };
